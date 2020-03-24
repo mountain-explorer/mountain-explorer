9591c296657c3a3bc2eda09be583516deb1c2320
LiYiBin <liyibin.tw@gmail.com>
2020-03-24 17:58:49 +0800

    wip

---
+++ _next/static/webpack/static/development/pages/index.js.0a3097bc81eb794fe171.hot-update.js
@@ -0,0 +1,279 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./node_modules/@material-ui/icons/Favorite.js":
+false,
+
+/***/ "./node_modules/@material-ui/icons/SwapHorizontalCircle.js":
+/*!*****************************************************************!*\
+  !*** ./node_modules/@material-ui/icons/SwapHorizontalCircle.js ***!
+  \*****************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _interopRequireDefault = __webpack_require__(/*! @babel/runtime/helpers/interopRequireDefault */ "./node_modules/@babel/runtime/helpers/interopRequireDefault.js");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
+
+var _createSvgIcon = _interopRequireDefault(__webpack_require__(/*! ./utils/createSvgIcon */ "./node_modules/@material-ui/icons/utils/createSvgIcon.js"));
+
+var _default = (0, _createSvgIcon.default)(_react.default.createElement("path", {
+  d: "M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12s4.48 10 10 10 10-4.48 10-10zm-7-5.5l3.5 3.5-3.5 3.5V11h-4V9h4V6.5zm-6 11L5.5 14 9 10.5V13h4v2H9v2.5z"
+}), 'SwapHorizontalCircle');
+
+exports.default = _default;
+
+/***/ }),
+
+/***/ "./src/components/Home/index.tsx":
+/*!***************************************!*\
+  !*** ./src/components/Home/index.tsx ***!
+  \***************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return ScrollableTabsButtonForce; });
+/* harmony import */ var _babel_runtime_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/esm/extends */ "./node_modules/@babel/runtime/helpers/esm/extends.js");
+/* harmony import */ var _babel_runtime_helpers_esm_slicedToArray__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime/helpers/esm/slicedToArray */ "./node_modules/@babel/runtime/helpers/esm/slicedToArray.js");
+/* harmony import */ var _babel_runtime_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @babel/runtime/helpers/esm/defineProperty */ "./node_modules/@babel/runtime/helpers/esm/defineProperty.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! next/head */ "./node_modules/next/dist/next-server/lib/head.js");
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var _material_ui_core_styles__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @material-ui/core/styles */ "./node_modules/@material-ui/core/esm/styles/index.js");
+/* harmony import */ var _material_ui_core_AppBar__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! @material-ui/core/AppBar */ "./node_modules/@material-ui/core/esm/AppBar/index.js");
+/* harmony import */ var _material_ui_core_Tabs__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! @material-ui/core/Tabs */ "./node_modules/@material-ui/core/esm/Tabs/index.js");
+/* harmony import */ var _material_ui_core_Tab__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! @material-ui/core/Tab */ "./node_modules/@material-ui/core/esm/Tab/index.js");
+/* harmony import */ var _material_ui_icons_Room__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! @material-ui/icons/Room */ "./node_modules/@material-ui/icons/Room.js");
+/* harmony import */ var _material_ui_icons_Room__WEBPACK_IMPORTED_MODULE_9___default = /*#__PURE__*/__webpack_require__.n(_material_ui_icons_Room__WEBPACK_IMPORTED_MODULE_9__);
+/* harmony import */ var _material_ui_icons_SwapHorizontalCircle__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! @material-ui/icons/SwapHorizontalCircle */ "./node_modules/@material-ui/icons/SwapHorizontalCircle.js");
+/* harmony import */ var _material_ui_icons_SwapHorizontalCircle__WEBPACK_IMPORTED_MODULE_10___default = /*#__PURE__*/__webpack_require__.n(_material_ui_icons_SwapHorizontalCircle__WEBPACK_IMPORTED_MODULE_10__);
+/* harmony import */ var _material_ui_icons_Explore__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! @material-ui/icons/Explore */ "./node_modules/@material-ui/icons/Explore.js");
+/* harmony import */ var _material_ui_icons_Explore__WEBPACK_IMPORTED_MODULE_11___default = /*#__PURE__*/__webpack_require__.n(_material_ui_icons_Explore__WEBPACK_IMPORTED_MODULE_11__);
+/* harmony import */ var _material_ui_core_Toolbar__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! @material-ui/core/Toolbar */ "./node_modules/@material-ui/core/esm/Toolbar/index.js");
+/* harmony import */ var _material_ui_core_Typography__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(/*! @material-ui/core/Typography */ "./node_modules/@material-ui/core/esm/Typography/index.js");
+/* harmony import */ var react_swipeable_views__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(/*! react-swipeable-views */ "./node_modules/react-swipeable-views/lib/index.js");
+/* harmony import */ var react_swipeable_views__WEBPACK_IMPORTED_MODULE_14___default = /*#__PURE__*/__webpack_require__.n(react_swipeable_views__WEBPACK_IMPORTED_MODULE_14__);
+/* harmony import */ var components_CoordinateCurrent__WEBPACK_IMPORTED_MODULE_15__ = __webpack_require__(/*! components/CoordinateCurrent */ "./src/components/CoordinateCurrent/index.tsx");
+/* harmony import */ var components_CoordinateCalculator__WEBPACK_IMPORTED_MODULE_16__ = __webpack_require__(/*! components/CoordinateCalculator */ "./src/components/CoordinateCalculator/index.tsx");
+/* harmony import */ var components_Compass__WEBPACK_IMPORTED_MODULE_17__ = __webpack_require__(/*! components/Compass */ "./src/components/Compass/index.tsx");
+/* harmony import */ var utils_constants__WEBPACK_IMPORTED_MODULE_18__ = __webpack_require__(/*! utils/constants */ "./src/utils/constants.ts");
+
+
+
+var _jsxFileName = "/Users/lyb/yb/explorer-src/src/components/Home/index.tsx";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_3___default.a.createElement;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function a11yProps(index) {
+  return {
+    id: "scrollable-force-tab-".concat(index),
+    "aria-controls": "scrollable-force-tabpanel-".concat(index)
+  };
+}
+
+var useStyles = Object(_material_ui_core_styles__WEBPACK_IMPORTED_MODULE_5__["makeStyles"])(function (theme) {
+  return {
+    root: {
+      flexGrow: 1,
+      width: "100%",
+      backgroundColor: theme.palette.background.paper
+    },
+    title: {
+      flexGrow: 1
+    },
+    content: Object(_babel_runtime_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_2__["default"])({
+      height: "calc(100vh - 72px - 56px)",
+      overflow: "scroll",
+      position: "relative"
+    }, "".concat(theme.breakpoints.up("sm")), {
+      height: "calc(100vh - 72px - 64px)"
+    })
+  };
+});
+function ScrollableTabsButtonForce() {
+  var classes = useStyles();
+
+  var _React$useState = react__WEBPACK_IMPORTED_MODULE_3___default.a.useState(0),
+      _React$useState2 = Object(_babel_runtime_helpers_esm_slicedToArray__WEBPACK_IMPORTED_MODULE_1__["default"])(_React$useState, 2),
+      tabIndex = _React$useState2[0],
+      setTabIndex = _React$useState2[1];
+
+  var handleChange = function handleChange(_, newValue) {
+    setTabIndex(newValue);
+  };
+
+  return __jsx("div", {
+    className: classes.root,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 60
+    },
+    __self: this
+  }, __jsx(_material_ui_core_AppBar__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    position: "static",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 61
+    },
+    __self: this
+  }, __jsx(_material_ui_core_Toolbar__WEBPACK_IMPORTED_MODULE_12__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 62
+    },
+    __self: this
+  }, __jsx(next_head__WEBPACK_IMPORTED_MODULE_4___default.a, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 63
+    },
+    __self: this
+  }, __jsx("title", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 64
+    },
+    __self: this
+  }, utils_constants__WEBPACK_IMPORTED_MODULE_18__["HEAD_TITLE"])), __jsx(_material_ui_core_Typography__WEBPACK_IMPORTED_MODULE_13__["default"], {
+    variant: "h6",
+    component: "h1",
+    className: classes.title,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 66
+    },
+    __self: this
+  }, utils_constants__WEBPACK_IMPORTED_MODULE_18__["HEAD_TITLE"]))), __jsx("div", {
+    className: classes.content,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 71
+    },
+    __self: this
+  }, __jsx(react_swipeable_views__WEBPACK_IMPORTED_MODULE_14___default.a, {
+    enableMouseEvents: true,
+    index: tabIndex,
+    onChangeIndex: function onChangeIndex(index) {
+      return setTabIndex(index);
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 72
+    },
+    __self: this
+  }, __jsx(components_CoordinateCurrent__WEBPACK_IMPORTED_MODULE_15__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 77
+    },
+    __self: this
+  }), __jsx(components_CoordinateCalculator__WEBPACK_IMPORTED_MODULE_16__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 78
+    },
+    __self: this
+  }), __jsx(components_Compass__WEBPACK_IMPORTED_MODULE_17__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 79
+    },
+    __self: this
+  }))), __jsx(_material_ui_core_AppBar__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    position: "static",
+    color: "default",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 82
+    },
+    __self: this
+  }, __jsx(_material_ui_core_Tabs__WEBPACK_IMPORTED_MODULE_7__["default"], {
+    value: tabIndex,
+    onChange: handleChange,
+    variant: "scrollable",
+    scrollButtons: "on",
+    indicatorColor: "primary",
+    textColor: "primary",
+    "aria-label": "scrollable force tabs",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 83
+    },
+    __self: this
+  }, __jsx(_material_ui_core_Tab__WEBPACK_IMPORTED_MODULE_8__["default"], Object(_babel_runtime_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_0__["default"])({
+    label: utils_constants__WEBPACK_IMPORTED_MODULE_18__["TAB_COORDINATE_CURRENT"],
+    icon: __jsx(_material_ui_icons_Room__WEBPACK_IMPORTED_MODULE_9___default.a, {
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 94
+      },
+      __self: this
+    })
+  }, a11yProps(0), {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 92
+    },
+    __self: this
+  })), __jsx(_material_ui_core_Tab__WEBPACK_IMPORTED_MODULE_8__["default"], Object(_babel_runtime_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_0__["default"])({
+    label: utils_constants__WEBPACK_IMPORTED_MODULE_18__["TAB_COORDINATE_CALCULATOR"],
+    icon: __jsx(_material_ui_icons_SwapHorizontalCircle__WEBPACK_IMPORTED_MODULE_10___default.a, {
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 99
+      },
+      __self: this
+    })
+  }, a11yProps(1), {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 97
+    },
+    __self: this
+  })), __jsx(_material_ui_core_Tab__WEBPACK_IMPORTED_MODULE_8__["default"], Object(_babel_runtime_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_0__["default"])({
+    label: utils_constants__WEBPACK_IMPORTED_MODULE_18__["TAB_COMPASS"],
+    icon: __jsx(_material_ui_icons_Explore__WEBPACK_IMPORTED_MODULE_11___default.a, {
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 102
+      },
+      __self: this
+    })
+  }, a11yProps(2), {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 102
+    },
+    __self: this
+  })))));
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.0a3097bc81eb794fe171.hot-update.js.map
---
+++ _next/static/webpack/static/development/pages/index.js.7ccbd0a941c74ba9d2c7.hot-update.js
@@ -0,0 +1,45 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./src/components/Compass/index.tsx":
+/*!******************************************!*\
+  !*** ./src/components/Compass/index.tsx ***!
+  \******************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _material_ui_core_Box__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @material-ui/core/Box */ "./node_modules/@material-ui/core/esm/Box/index.js");
+var _jsxFileName = "/Users/lyb/yb/explorer-src/src/components/Compass/index.tsx";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  Object(react__WEBPACK_IMPORTED_MODULE_0__["useEffect"])(function () {
+    if (window.DeviceOrientationEvent) {
+      window.addEventListener("deviceorientation", function () {
+        for (var _len = arguments.length, data = new Array(_len), _key = 0; _key < _len; _key++) {
+          data[_key] = arguments[_key];
+        }
+
+        console.log(data);
+      }, false);
+    }
+  }, []);
+  return __jsx(_material_ui_core_Box__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    m: 1,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17
+    },
+    __self: this
+  }, "TODO");
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.7ccbd0a941c74ba9d2c7.hot-update.js.map
